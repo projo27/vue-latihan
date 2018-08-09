96862be924a208376779451ee50b82dc080b9f40
projo27 <projo27@gmail.com>
2018-08-09 20:42:16 +0700

    tambah register

---
+++ client/src/components/Register.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+   <h1>Register</h1>
+   <input type="email" name="email" placeholder="email" v-model="email" /><br />
+   <input type="password" name="password" placeholder="password" v-model="password"><br />
+   <button @click="register">Register</button>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '../../services/AuthenticationService'
+export default {
+  name: "Register",
+  data() {
+    return {
+      email: "email",
+      password: "password"
+    };
+  },
+  watch: {
+    email (value) {
+      console.log("email berubah", value)
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      this.email = "hello world";
+    }, 2000);
+  },
+  methods: {
+    async register (){
+      const response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log(response.data)
+    }
+    // register() {
+    //   console.log("tombol register telah terpilih", this.email, this.password);
+    // }
+  }
+};
+</script>
+
+
+<style scoped>
+</style>
